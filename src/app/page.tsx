8d6004822268c7c62850da6aec903674c82cd28a
Paulo Victor <paulovictor@MacBook-Pro-de-Paulo.local>
2025-07-28 20:50:33 -0300

    Feat: Redireciona página inicial para login/dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
-          Projeto Gênesis
-        </h1>
-        <p className="mt-4 text-lg text-gray-500 md:text-xl dark:text-gray-400">
-          Nossa incrível plataforma de criação de sites está chegando em breve.
-        </p>
-      </div>
-    </main>
-  );
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect("/dashboard");
+  } else {
+    redirect("/login");
+  }
 }
