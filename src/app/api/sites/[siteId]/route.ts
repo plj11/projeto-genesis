d155a0e7854316cd028f16c7cecb99803584b7a8
Paulo Victor <paulovictor@MacBook-Pro-de-Paulo.local>
2025-08-04 18:30:07 -0300

    Debug: Altera tipo de parâmetro para any na rota de sites

--- src/app/api/sites/[siteId]/route.ts
+++ src/app/api/sites/[siteId]/route.ts
@@ -3,7 +3,7 @@ import prisma from '@/lib/prisma';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/lib/auth';
 
-export async function POST(req: NextRequest, context: { params: { siteId: string } }) {
+export async function POST(req: NextRequest, context: any) {
   const siteId = context.params.siteId;
   const session = await getServerSession(authOptions);
   if (!session || !session.user) {
