df4c794a824d5018cdaadc2a76b8566b7f2e5ffe
Paulo Victor <paulovictor@MacBook-Pro-de-Paulo.local>
2025-09-24 21:29:33 -0300

    fix: Remove duplicate Credentials import

--- packages/website/src/auth.ts
+++ packages/website/src/auth.ts
@@ -1,7 +1,6 @@
 import NextAuth from 'next-auth'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import Credentials from 'next-auth/providers/credentials'
-import Credentials from 'next-auth/providers/credentials'
 import Google from 'next-auth/providers/google'
 import GitHub from 'next-auth/providers/github'
 import { authConfig } from './auth.config'
